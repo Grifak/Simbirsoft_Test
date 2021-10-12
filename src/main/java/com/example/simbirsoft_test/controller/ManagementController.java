67cb2f1b6986b6e4f8076437fa6115f659b8b7d0
Grifak <vadim-nepomnyaschii@mail.ru>
2021-10-13 00:23:56 +0300

    inject wordService to ManagementController

--- src/main/java/com/example/simbirsoft_test/controller/ManagementController.java
+++ src/main/java/com/example/simbirsoft_test/controller/ManagementController.java
@@ -1,18 +1,21 @@
 package com.example.simbirsoft_test.controller;
 
+import com.example.simbirsoft_test.service.WordService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping("/api/management")
+@RequiredArgsConstructor
 @RestController
 public class ManagementController {
+    private final WordService wordService;
 
     @GetMapping("/download")
     public ResponseEntity download(HttpServletResponse servletResponse){
-        //TODO скачать файл
-        Boolean result = true;
+        Boolean result = wordService.downloadFile(servletResponse);
 
         return ResponseEntity.ok().body(result);
     }
