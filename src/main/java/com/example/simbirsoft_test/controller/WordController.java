68b08b2747fc08bd6fa2ae0f59b206b24dc0e927
Grifak <vadim-nepomnyaschii@mail.ru>
2021-10-12 15:05:55 +0300

    inject wordService to WordController

--- src/main/java/com/example/simbirsoft_test/controller/WordController.java
+++ src/main/java/com/example/simbirsoft_test/controller/WordController.java
@@ -1,19 +1,22 @@
 package com.example.simbirsoft_test.controller;
 
+import com.example.simbirsoft_test.service.WordService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/word")
 public class WordController {
+    private final WordService wordService;
 
     @PostMapping("/create")
     public ResponseEntity createWords(@RequestParam("link") String link){
-        //TODO прочитать страницу и заполнить БД
-        Boolean result = true;
+        Boolean result = wordService.createWords(link);
 
         return ResponseEntity.ok().body(result);
     }
