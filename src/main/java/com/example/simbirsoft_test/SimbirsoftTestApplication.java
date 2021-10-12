3dff7c8fb0fa95864122b91203deff28165af9d4
Grifak <vadim-nepomnyaschii@mail.ru>
2021-10-12 11:51:41 +0300

    Initial commit

---
+++ src/main/java/com/example/simbirsoft_test/SimbirsoftTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.simbirsoft_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirsoftTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirsoftTestApplication.class, args);
+    }
+
+}
