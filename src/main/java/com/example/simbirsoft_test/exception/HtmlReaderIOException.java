32e27c27a946f4a91bcf28a731b8f1fc671123f0
Grifak <vadim-nepomnyaschii@mail.ru>
2021-10-12 15:03:02 +0300

    create HtmlReaderIOException

---
+++ src/main/java/com/example/simbirsoft_test/exception/HtmlReaderIOException.java
@@ -0,0 +1,7 @@
+package com.example.simbirsoft_test.exception;
+
+public class HtmlReaderIOException extends RuntimeException{
+    public HtmlReaderIOException() {
+        super("HtmlReader IOException");
+    }
+}
